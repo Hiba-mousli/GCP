2fe6ae09891e99d13b0213ff573baebce7f53854
Hiba-mousli <81761921+Hiba-mousli@users.noreply.github.com>
2021-06-19 19:31:43 +0300

    -- HIBA

--- src/main/java/org/closure/gcp/repositories/AnswerRepo.java
+++ src/main/java/org/closure/gcp/repositories/AnswerRepo.java
@@ -1,13 +1,12 @@
 package org.closure.gcp.repositories;
 
 import java.util.Optional;
+
 import org.closure.gcp.entities.AnswerEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AnswerRepo extends CrudRepository<AnswerEntity,Integer> {
-
-    Optional<AnswerEntity> findByAnswer(String answer);
-
+Optional <AnswerEntity> findById(int id );
 }
